2fceb3a256a215239dc431f390377188c931a194
Camilo <Camiloblancoamado@gmail.com>
2024-03-02 18:55:28 -0500

    Mouredev retos de programacion

---
+++ Primero.py
@@ -0,0 +1,32 @@
+#Url retos de programacion = https://retosdeprogramacion.com/roadmap/#last
+
+#Este es un comentario de una linea
+
+"""
+Este es un comentario de
+varias lineas
+utilizando comillas dobles
+
+"""
+'''
+Este es un comentario de
+varias lineas
+utilizando comillas simples
+'''
+
+#variable y constante
+variable = ("Hola mundo")
+constante = ("60 segundos ")
+
+#tipos de datos en python
+print(type("python es genial"))#dato tipo str que representa una cadena de texto
+print(type(2))#dato tipo int que representa datos como numeros 
+print(type(3.16))#dato tipo float que representa numeros decimales
+print(type(False))#dato tipo bool que representa datos como verdadero o falso muy util para hacer comparaciones
+print(type(list))#dato tipo type que representa listas
+print(type({1,2,3}))#dato tipo set
+tuple = (1,2,3,4,5,)#Lista tipo tuple
+print(type(tuple))#Lista tipo tuple
+
+#Imprimir por terminal
+print("Hola python")
